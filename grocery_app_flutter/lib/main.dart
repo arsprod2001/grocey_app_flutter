2ad2a1970c2395bf21212594ee17fc3fe1ac0da9
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-12-04 14:16:21 -0500

    Configuration main

--- grocery_app_flutter/lib/main.dart
+++ grocery_app_flutter/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/cart_screen.dart';
+import 'screens/notifications_screen.dart';
+import 'screens/profile_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +15,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Mon Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const AuthWrapper(),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(
-        onLogin: () {
-          print("Retour à la page Login");
-        },
+    );
+  }
+}
+
+class AuthWrapper extends StatefulWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  bool _isLoggedIn = false;
+
+  void _login() {
+    setState(() {
+      _isLoggedIn = true;
+    });
+  }
+
+  void _logout() {
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoggedIn 
+        ? MainNavigation(onLogout: _logout)
+        : LoginScreen(onLogin: _login);
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  final VoidCallback onLogout;
+  
+  const MainNavigation({super.key, required this.onLogout});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const CartScreen(),
+    const NotificationsScreen(),
+    ProfileScreen(), 
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    _screens[3] = ProfileScreen();
+    
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Panier',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
